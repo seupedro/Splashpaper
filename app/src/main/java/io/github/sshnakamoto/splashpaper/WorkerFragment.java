66767f01e6c55729370c992967ad5e1f00a0fb8c
nakamoto <hartmnn.p@gmail.com>
2018-09-01 16:49:52 -0300

    remove OnResume Code

--- app/src/main/java/io/github/sshnakamoto/splashpaper/WorkerFragment.java
+++ app/src/main/java/io/github/sshnakamoto/splashpaper/WorkerFragment.java
@@ -25,7 +25,9 @@ public class WorkerFragment extends Fragment {
     private static final String TAG = "WorkerFragment";
 
     String UrlApi = "https://images.unsplash.com/photo-1511736515797-8aab81ec7e35?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=966f3c728ebb930b49192fdbf14b55e0&auto=format&fit=crop&w=1534&q=80";
-//    String UrlApi = "https://images.unsplash.com/photo-1535632788826-78ca9d09d2e7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bb651ad5967f2074c98b8c30ae2fc442&auto=format&fit=crop&w=695&q=80";
+    private ImageView imageView;
+    private TextView textView;
+    //    String UrlApi = "https://images.unsplash.com/photo-1535632788826-78ca9d09d2e7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bb651ad5967f2074c98b8c30ae2fc442&auto=format&fit=crop&w=695&q=80";
 
     public WorkerFragment() {
         // Required empty public constructor
@@ -38,16 +40,9 @@ public class WorkerFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_worker, container, false);
 
-        final ImageView imageView = rootView.findViewById(R.id.imageView);
+        imageView = rootView.findViewById(R.id.imageView);
         TextView textView = rootView.findViewById(R.id.textView);
 
-        WorkerTask task = new WorkerTask(getContext(), imageView, new WallpaperListener() {
-            @Override
-            public void setWallpaper(Bitmap bitmap) {
-                imageView.setImageBitmap(bitmap);
-            }
-        });
-        task.execute(UrlApi);
         return rootView;
     }
 
@@ -55,4 +50,5 @@ public class WorkerFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
+    
 }
